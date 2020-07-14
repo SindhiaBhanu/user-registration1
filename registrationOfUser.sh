cc90496b2e472cc24b1162e83eab3bd70aca7dbb
SindhiaBhanu <sindhiabhanu518@gmail.com>
2020-07-15 01:34:47 +0530

    added code for entering the mail of user

--- registrationOfUser.sh
+++ registrationOfUser.sh
@@ -1,14 +1,13 @@
 #!/bin/bash -x
 shopt -s extglob
-read lastName
-pattern="^[A-Z]{1}[a-z]{3,}$"
-if [[ $lastName =~ $pattern ]]
+read char
+pattern="^[a-zA-Z0-9]+([.+_-]{1}[a-zA-Z0-9]{3,}){0,2}\@([a-zA-Z0-9]+\.[a-z]+){2,4}"
+if [[ $char =~ $pattern ]]
 then
-        echo "$lastName"
+        echo yes
 else
-        echo "invalid"
+        echo no
 fi
-
 
 
 
